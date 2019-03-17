03b5bccf18e0fbab318406f5dd74e8ec415c267b
Krzysztof Mazurkiewicz <k3mazurkiewicz@gmail.com>
2019-03-17 11:25:37 +0100

    Change all fields to mandatory in register new user.

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServicesService } from './../services.service';
 import { LoaderService } from './../loader.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
@@ -13,17 +14,25 @@ export class NewUserComponent {
   password;
   login;
 
+  formGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    login: new FormControl('', [Validators.required])
+  })
+
+
   constructor(private services: ServicesService,
               private spinner: LoaderService, 
               private router: Router) { }
 
   public onRegisterClick(): void {
     this.spinner.turnOn();
-    const newUser = {
-      name: this.name,
-      password: this.password,
-      login: this.login
-    }
+    const newUser = this.formGroup.getRawValue();
+    // const newUser = {
+    //   name: this.formGroup.get('name').value,
+    //   password: this.formGroup.get('password').value,
+    //   login: this.login
+    // }
     this.services.getAllUsers().subscribe((users: any[]) => {
       const findedUser = users.find((user) => user.login === this.login);
       if (findedUser) {
